ccbd5c3886df273942404fc3455a1362250c9bb7
Anastasiia Prokopeva <anastasiiaprokopeva31@gmail.com>
2023-05-17 14:42:48 +0500

    update

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,21 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App/App";
-import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "store";
-import ErrorBoundary from "Components/ErrorBoundary/ErrorBoundary";
+ import ReactDOM from "react-dom/client";
+ import "./index.css";
+ import App from "./App/App";
+ import { BrowserRouter } from "react-router-dom";
+ import { Provider } from "react-redux";
+ import { store } from "store";
+ import ErrorBoundary from "Components/ErrorBoundary/ErrorBoundary";
 
-const root = ReactDOM.createRoot(
-	document.getElementById("root") as HTMLElement
-);
-root.render(
-	<Provider store={store}>
-		<BrowserRouter>
-			<ErrorBoundary>
-				<App />
-			</ErrorBoundary>
-		</BrowserRouter>
-	</Provider>
-);
+ const root = ReactDOM.createRoot(
+ 	document.getElementById("root") as HTMLElement
+ );
+ root.render(
+ 	<Provider store={store}>
+ 		<BrowserRouter>
+ 			<ErrorBoundary>
+ 				<App />
+ 			</ErrorBoundary>
+ 		</BrowserRouter>
+ 	</Provider>
+ );
