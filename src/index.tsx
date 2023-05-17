2ede37465774725f3c67046a2a81c51886ff294e
Anastasiia Prokopeva <anastasiiaprokopeva31@gmail.com>
2023-05-17 14:35:01 +0500

    update

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App/App";
-import { BrowserRouter, HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "store";
 import ErrorBoundary from "Components/ErrorBoundary/ErrorBoundary";
@@ -12,10 +12,10 @@ const root = ReactDOM.createRoot(
 );
 root.render(
 	<Provider store={store}>
-		<HashRouter>
+		<BrowserRouter>
 			<ErrorBoundary>
 				<App />
 			</ErrorBoundary>
-		</HashRouter>
+		</BrowserRouter>
 	</Provider>
 );
